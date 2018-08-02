276a7a7ebb83ec3dd07e3e4edd755c8913a744d0
irelatedggx2 <irelatedggx2@gmail.com>
2018-08-02 13:49:23 +0200

    simple battle (console/text) with user input

---
+++ pkmn.js
@@ -0,0 +1,17 @@
+function logMe() {
+	console.log('works');
+}
+
+window.onload = function(){
+	logME();
+}
+// gameEnded=0;
+
+// while(gameEnded!=1){
+// 	function fight(){
+
+// 	gameEnded=1;
+// 	return gameEnded;	
+// 	}
+// }
+// console.log("walka dobiegła końca");
